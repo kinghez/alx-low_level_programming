873bc9b7bf5711701482d9546cddf5282aee6d32
kinghez <kinghez10@gmail.com>
2022-09-09 11:35:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                      
+
+/**
+ * main - this function output with puts
+ *
+ *Description: using the main function
+ *this programm prints \"Programming is like building a multilingual puzzle
+ *return: 0
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
