bcb83763f438c28bd33ec60578f87b8e118968a6
kinghez <kinghez10@gmail.com>
2022-09-09 11:42:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
 /**
  * main - this function output with puts
