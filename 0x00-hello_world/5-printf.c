be5dd26060ea9cdf1999f6fe889931dc75885590
kinghez <kinghez10@gmail.com>
2022-09-09 11:52:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - this function uses puts to print
+ *
+ *Desscription: it print the statement
+ *with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
