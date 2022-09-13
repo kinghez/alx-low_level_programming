c1a537ddb661b93bc9263b40e2c817023cb65023
kinghez <kinghez10@gmail.com>
2022-09-12 18:13:21 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- *main - print if the number is positive, zero or negative
- *Description: using the main function 
- *this programme print if a number is positive, zero or negative
- *
- *Return: 0
+ * main - print if the number is postive, zero, or negative
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes here */
-	if (n > 0)
-	{
-		printf('%d is positive\n', n);
-	}
-	else if (n == 0)
-	{
-		printf('%d is zero\n', n);
-	}
-	else if (n < 0)
-	{
-		printf('%d is negative\n', n);
-	}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+	printf("%i is positive\n", n);
+
+}
+else if (n == 0)
+{
+	printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+	printf("%i is negative\n", n);
+}
+return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - this is the main function
+ *
+ *Description: this programme uses the main function
+ *to print all numbers in base ten
+ *
+ * Return: 0
+ */
+int main(viod)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+	printf('%d', n);
+}
+printf('\n');
+return (0);
+}
