5281a5b2549c9117b2e97f9a4f90d3ea06b39a52
kinghez <kinghez10@gmail.com>
2022-09-14 06:38:25 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - a function that check in a number is lower
+ *@c: is int that will be use for the arhument of the function
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
