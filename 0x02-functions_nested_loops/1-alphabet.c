3707e6b1352983e82117dec3c207eb64ed0cd0a2
kinghez <kinghez10@gmail.com>
2022-09-14 05:58:14 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-*main - the main function call the print alpha function
-*Description: this programme uses the main function
-*to print all alphabet using the alpha fuct
-*Return: 0
+*print_alphabet - function to print abc
+*
+* Return: 0
 */
 void print_alphabet(void)
 {
