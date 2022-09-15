c26e93b8323fca15f7af6f13678fbc7ac15bce59
kinghez <kinghez10@gmail.com>
2022-09-15 08:26:42 -0700

    0-main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - this function print integers
+ * Description: using the main function
+ * this programme print negative and positive integers
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
