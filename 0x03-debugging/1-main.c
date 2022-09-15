824b5204e7dbebd0ed18c880907203e5c1628b69
kinghez <kinghez10@gmail.com>
2022-09-15 08:50:28 -0700

    1-main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this function makes the programme works
+ *
+ * Description: from the main function
+ * this programme runs as it was programmed
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+printf("infinite loop incoming: (\n");
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ *	putchar(i);
+ * }
+ */
+printf("infinite loop avoided\\o/\n");
+return (0);
+}
