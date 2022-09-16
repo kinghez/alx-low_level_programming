65368003fac2aaa0ba4190c52cc084996865a090
kinghez <kinghez10@gmail.com>
2022-09-15 19:47:13 -0700

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,12 +5,14 @@
  * print_line  - print a straight line
  * @n: number of _ printed
  * Return: nothing
+ *
  */
+
 void print_line(int n)
 {
-while (n-- > 0)
-{
-putchar('_');
-}
-return ('\n');
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
 }
