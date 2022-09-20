60e4734609a1f7f191afc68a849f4291027c5957
kinghez <kinghez10@gmail.com>
2022-09-19 17:38:22 -0700

    pointers_arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function reset the argument to 98
+ *
+ * @n: this is a pointer qrgument
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
