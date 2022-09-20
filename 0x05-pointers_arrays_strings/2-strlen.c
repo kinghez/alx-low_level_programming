51db70211c4f072ed4d7fc356276b812f3c1d758
kinghez <kinghez10@gmail.com>
2022-09-19 20:11:13 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - this function print the length te of a character
+ *
+ * @s: the parameter for the function
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
